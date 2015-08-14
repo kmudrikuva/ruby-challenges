9102d4a1e65bffaff9e9d5bed74540c2d0b929b3
kmudrikuva <za.katie@gmail.com>
2015-08-14 15:51:23 +0300

    My first commit yaay

---
+++ ruby_challenges/symbols.rb
@@ -0,0 +1,13 @@
+user1 = {name:"Katie", plan:"freetrial"}
+user2 = {name:"Rossy", plan:"premium"} 
+user3 = {name:"Charly", plan:"freetrail"} 
+user4 = {name:"Moen", plan:"premium"} 
+user5 = {name:"Abdul", plan:"beta"} 
+user6 = {name:"Sahsha", plan:"complete"} 
+user7 = {name:"Olesya", plan:"beta"} 
+user8 = {name:"Lyuda", plan:"freetrial"} 
+user9 = {name:"Sonya", plan:"beta"} 
+ 
+freetrail_user = user1[:name]
+
+puts"  User which has freetrail is #{freetrail_user}"
